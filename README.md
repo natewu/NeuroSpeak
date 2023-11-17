ba595c136f5237f350ecb722790d8b3e93a87577
neilblaze <putubanerjee23@gmail.com>
2023-11-18 01:09:45 +0530

    [code]: backend :pizza:
    TODO: Prompt Fine-Tuning

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # NATHacks23
+
+> [!NOTE]
+For backend, go to the **gpt-backend** folder and run `npm install` to install all the dependencies. Then run `npm start` to start the server.
+
+Don't forget to add the API key inside the `.env` file.
---
+++ gpt-backend/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const OpenAI = require("openai");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+app.use(express.json());
+
+app.post("/suggest-keywords", async (req, res) => {
+  try {
+    const { dataInput } = req.body;
+
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "Understand the context from the input and provide a limited set of single-worded response keywords." },
+        { role: "user", content: `Context: ${dataInput}` },
+        { role: "assistant", content: "Keywords: " },
+      ],
+      model: "gpt-3.5-turbo",
+    });
+
+    const keywords = completion.choices[0].message.content.trim();
+    res.json({ keywords });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
