b0dd5fca6299caafd71b7662ecfed50dd5e40d10
natewu <36091727+natewu@users.noreply.github.com>
2023-11-18 04:15:42 -0700

    redux for phrases

--- frontend/src/components/PhraseContainer/PhraseBox/PhraseBox.tsx
+++ frontend/src/components/PhraseContainer/PhraseBox/PhraseBox.tsx
@@ -1,12 +1,25 @@
-import styles from './PhraseBox.module.scss';
+import { Phrase, select } from 'redux/reducers/phrasesSlice';
 
-export default function PhraseBox({phrases}: {phrases: string[]}) {
+import styles from './PhraseBox.module.scss';
+import { useDispatch } from "react-redux";
+
+export default function PhraseBox({phrases}: {phrases: Phrase[]}) {
+   const dispatch = useDispatch();
+
+   // set selected phrase
+   function selectPhrase(index: number) {
+      dispatch(select(index));
+      
+   }
    return (
       <div className={styles.PhraseBox}>
          <div className={styles.container}>
             {phrases.map((phrase, index) => (
-               <div key={index} className={styles.phrase}>
-                  <p>{phrase}</p>
+               <div key={index} className={styles.phrase}
+                  style={{backgroundColor: `${phrase.selected ? "#31BAD2" : "rgba(128, 128, 128, 0.1)"}`}}
+                  onClick={() => selectPhrase(phrase.id)}
+               >
+                  <p>{phrase.phrase}</p>
                </div>
             ))}
          </div>
--- frontend/src/components/PhraseContainer/PhraseSelector/PhraseSelector.tsx
+++ frontend/src/components/PhraseContainer/PhraseSelector/PhraseSelector.tsx
@@ -4,17 +4,17 @@ import PhraseBox from "../PhraseBox/PhraseBox";
 import styles from './PhraseSelector.module.scss';
 
 type PhraseContainerProps = {
-   phrasebox: any[2];
+   phrasebox: any[4];
    style?: any;
  };
 
 export default function PhraseSelector({ phrasebox, style }: PhraseContainerProps) {
    return (
       <div className={styles.wrapper} style={style}>
-            <PhraseBox phrases={phrasebox[0]} />   
+            <PhraseBox phrases={phrasebox} />   
             {/* Separator line */}
             <div className="line"/>
-            <PhraseBox phrases={phrasebox[1]} />   
+            {/* <PhraseBox phrases={phrasebox[1]} />    */}
       </div>
    );
 }
