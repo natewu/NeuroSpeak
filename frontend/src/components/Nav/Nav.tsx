f4aeeb0b4cc433b32d175fd24e0ff3470f821a42
neilblaze <putubanerjee23@gmail.com>
2023-11-18 17:14:23 +0530

    (client): Speech2Text module

--- frontend/src/components/Nav/Nav.tsx
+++ frontend/src/components/Nav/Nav.tsx
@@ -1,13 +1,56 @@
+import { useState, useEffect } from 'react';
 import MicIcon from '@mui/icons-material/Mic';
 import styles from './Nav.module.scss';
+import 'regenerator-runtime/runtime';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-export default function Nav() {
-   return (
-      <div className={styles.wrapper}>
-         <h1 className={styles.name}>
-            NeuroSpeak
-         </h1>
-         <MicIcon className={styles.mic} />
+const App = () => {
+  const [isListening, setIsListening] = useState(false);
+
+  const toggleListening = () => {
+    if (isListening) {
+      SpeechRecognition.stopListening();
+    } else {
+      SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
+      resetTranscript();
+      // Automatically stop listening after 10 seconds
+      setTimeout(() => {
+        SpeechRecognition.stopListening();
+        setIsListening(false);
+      }, 10000);
+    }
+    setIsListening(!isListening);
+  };
+
+  const { transcript, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition();
+
+  useEffect(() => {
+    console.log(transcript); // Log transcript to console whenever it changes
+  }, [transcript]);
+
+  if (!browserSupportsSpeechRecognition) {
+    return <div>Speech recognition not supported</div>;
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.name}>NeuroSpeak</h1>
+
+      <div>
+        {/* <p>{transcript}</p> */}
       </div>
-   );
-}
+
+      <div className={styles.customButton} onClick={toggleListening}>
+        <MicIcon
+          className={styles.mic}
+          style={{
+            fontSize: '26px',
+            color: isListening ? 'red' : 'black',
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
