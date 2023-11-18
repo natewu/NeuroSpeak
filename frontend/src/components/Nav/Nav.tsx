d0c2aff6c7f7acbac107d8e95cbf71801e9b3b7f
natewu <36091727+natewu@users.noreply.github.com>
2023-11-17 17:41:02 -0700

    ui 🚀

--- frontend/src/components/Nav/Nav.tsx
+++ frontend/src/components/Nav/Nav.tsx
@@ -1,11 +1,13 @@
+import MicIcon from '@mui/icons-material/Mic';
 import styles from './Nav.module.scss';
 
 export default function Nav() {
    return (
       <div className={styles.wrapper}>
          <h1 className={styles.name}>
-            NAME
+            NeuroSpeak
          </h1>
+         <MicIcon className={styles.mic} />
       </div>
    );
 }
